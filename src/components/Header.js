2170fe183c81277f71a09cde449765fb9581bea3
vinaykumar785557 <c0785557@mylambton.ca>
2022-04-23 21:50:26 -0400

    added routes to homepage and menu page

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,11 @@
 import { ShoppingBag } from '@mui/icons-material';
 import React, { useState } from 'react';
+import { useHistory } from 'react-router-dom/cjs/react-router-dom.min';
 import '../styles/Header.css';
 import CartModal from './CartModal';
 import SubNav from './SubNav';
 function Header() {
+	const history = useHistory();
 	const [show, setShow] = useState(false);
 	const [showCart, setShowCart] = useState(false);
 
@@ -13,11 +15,22 @@ function Header() {
 		setShowCart(!showCart);
 	};
 
+	const menupage = () => {
+		history.push('/menu');
+	};
+	const homepage = () => {
+		history.push('/');
+	};
+
+	const signinpage = () => {
+		history.push('/signin');
+	};
+
 	return (
 		<div className='header'>
 			<div className='header__left'>
 				<ul className='header__left__items'>
-					<li>Menu</li>
+					<li onClick={menupage}>Menu</li>
 					<li>Tims for Good</li>
 					<li onClick={subMenuHandler}>More &#x25BC;</li>
 				</ul>
@@ -26,10 +39,13 @@ function Header() {
 			<img
 				src='https://www.serrala.com/sites/default/files/styles/max_650x650/public/2021-05/luxpng.com_tim-hortons-logo-5403680.png?itok=7Po9iWUU'
 				alt='tim-hortons-logo'
-				className='header__logo'></img>
+				className='header__logo'
+				onClick={homepage}></img>
 
 			<div className='header__right'>
-				<div className='button__signin'>Sign In</div>
+				<div className='button__signin' onClick={signinpage}>
+					Sign In
+				</div>
 				<div className='button__cart' onClick={showCartHandler}>
 					<ShoppingBag />
 					<p className='price'>0</p>
